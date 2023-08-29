0e03caae407924572b1321536008e084614b3629
urielbang <uri32655@gmail.com>
2023-08-29 12:56:55 +0300

    js

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 'use strict';
-console.log(document.querySelector('.message').textContent);
+
+const secretNumber = Math.trunc(Math.random() * 10) + 1;
+document.querySelector('.number').textContent = secretNumber;
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess, guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number !';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'correct Number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too high';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too low!';
+  }
+});
