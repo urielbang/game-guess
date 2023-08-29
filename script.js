0cd926727569bbe9095f0a9a7f56f072bbd1d088
urielbang <uri32655@gmail.com>
2023-08-29 13:03:26 +0300

    score number

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const secretNumber = Math.trunc(Math.random() * 10) + 1;
+let score = 20;
 document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -12,7 +13,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'correct Number!';
   } else if (guess > secretNumber) {
     document.querySelector('.message').textContent = 'Too high';
+    score--;
+    document.querySelector('.score').textContent = score;
   } else if (guess < secretNumber) {
     document.querySelector('.message').textContent = 'Too low!';
+    score--;
+    document.querySelector('.score').textContent = score;
   }
 });
