9fddbf192295ea05098cd8ee3bb4e03d35fb3f4d
urielbang <uri32655@gmail.com>
2023-09-18 11:29:01 +0300

    game changes

--- script.js
+++ script.js
@@ -5,17 +5,14 @@ let score = 20;
 // document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  // console.log(typeof guess, guess);
+
   let max = 0;
   if (!guess) {
     document.querySelector('.message').textContent = 'No number !';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'correct Number!';
     document.body.style.backgroundColor = 'green';
-    if (score > max) {
-      max = score;
-    }
-    document.querySelector('.highscore').textContent = max;
+    document.querySelector('.number').innerHTML = guess;
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too high';
@@ -23,7 +20,7 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = score;
     } else {
       document.querySelector('.message').textContent = 'you lost the game!';
-      document.querySelector('.score').textContent = score;
+      document.querySelector('.score').textContent = 0;
     }
   } else if (guess < secretNumber) {
     if (score > 1) {
@@ -38,7 +35,7 @@ document.querySelector('.check').addEventListener('click', function () {
 });
 document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.score').textContent = 20;
-  document.querySelector('.highscore').textContent = 0;
+
   document.querySelector('.message').textContent = 'Start guessing...';
   document.body.style.backgroundColor = '#222';
   document.querySelector('.guess').value = '';
